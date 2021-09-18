90d8831b495af9c094f4e0042e70215e2fb351bb
Toàn Trần <neo290992@gmail.com>
2021-09-18 18:26:23 +0700

    capnhat1

--- my.js
+++ my.js
@@ -1,16 +1,17 @@
 let X=0
+document.getElementById("dongy").style.display= 'none'
 function setRandomPosition(){
     document.getElementById("btn-no").style.top = Math.floor(Math.random() * 200) + "px";
     document.getElementById("btn-no").style.left = Math.floor(Math.random() * 1000) + "px";
     switch (X) {
         case 7:
-            document.getElementById("content").innerHTML="Thôi đừng từ chối nữa, bấm nút còn lại nào^^"
+            document.getElementById("title").innerHTML="Thôi đừng từ chối nữa, bấm nút còn lại nào^^"
         break;
         case 9:
-            document.getElementById("content").innerHTML="Haizz, anh bảo rồi!!Đừng cố chấp nữa, Bấm nút còn lại nào^^!!"
+            document.getElementById("title").innerHTML="Haizz, anh bảo rồi!!Đừng cố chấp nữa, Bấm nút còn lại nào^^!!"
         break;
-        case 20:
-            document.getElementById("content").innerHTML="Giờ từ chối thử xem^^ hehe"
+        case 15:
+            document.getElementById("title").innerHTML="Giờ từ chối thử xem^^ hehe"
             document.getElementById("btn-no").style.display= 'none'
         break;
     
@@ -19,5 +20,7 @@ function setRandomPosition(){
     X++
     }
 function yess(){
- document.write("Cảm ơn em")
+    document.getElementById("dongy").style.display= 'block'
+    document.getElementById("totinh").style.display= 'none'
+    document.getElementById("nhac").innerHTML='<iframe src="https://www.nhaccuatui.com/mh/background/2hI4xFTa2lxJ" width="1" height="1" frameborder="0" allowfullscreen allow="autoplay"></iframe>'
 }
