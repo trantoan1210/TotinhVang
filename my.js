5441ece4eddaa872fc9ba5d8a041b6b53a03cb97
Toàn Trần <neo290992@gmail.com>
2021-09-28 22:34:54 +0700

    capnhat

--- my.js
+++ my.js
@@ -1,8 +1,8 @@
 let X=0
 document.getElementById("dongy").style.display= 'none'
 function setRandomPosition(){
-    document.getElementById("btn-no").style.top = Math.floor(Math.random() * 200) + "px";
-    document.getElementById("btn-no").style.left = Math.floor(Math.random() * 1000) + "px";
+    document.getElementById("btn-no").style.top = Math.floor(Math.random() * 100) + "%";
+    document.getElementById("btn-no").style.left = Math.floor(Math.random() * 100) + "%";
     switch (X) {
         case 7:
             document.getElementById("title").innerHTML="Thôi đừng từ chối nữa, bấm nút còn lại nào^^"
@@ -22,5 +22,4 @@ function setRandomPosition(){
 function yess(){
     document.getElementById("dongy").style.display= 'block'
     document.getElementById("totinh").style.display= 'none'
-    document.getElementById("nhac").innerHTML='<iframe src="https://www.nhaccuatui.com/mh/background/2hI4xFTa2lxJ" width="1" height="1" frameborder="0" allowfullscreen allow="autoplay"></iframe>'
-}
+    }
